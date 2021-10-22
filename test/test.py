773200e774b2206bf936175258343e5b2a24ae5c
alsdn82 <vhrcnsehr@naver.com>
2021-10-22 12:57:37 +0900

    pyworks

---
+++ test/test.py
@@ -0,0 +1,24 @@
+# 1
+
+
+
+
+
+
+
+
+# 2
+y = int(input("년도를 입력하세요: "))
+w = ['월', '화', '수', '목', '금', '토', '일']
+if
+print(y)
+
+
+
+
+
+
+for i in range(2, 10):
+    for j in range(1, 10):
+        print("%d x %d = %d" % (i, j, i*j))
+    print()
